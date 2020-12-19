e3c9de0797fff55beb331cf7f3f1859e9278625c
WangWen <30790888+encounter1997@users.noreply.github.com>
2020-12-19 15:47:21 +0800

    copy

--- trainval_net.py
+++ trainval_net.py
@@ -107,8 +107,8 @@ if __name__ == '__main__':
     raise ValueError("duplicate output dir !")
 
   # save trainval_net.py and lib/models/faster_rcnn
-  shutil.copyfile('trainval_net.py', os.path.join(output_dir, 'train_files'))  # todo: modify this line when renaming the file
-  shutil.copyfile('lib/model/faster_rcnn/*', os.path.join(output_dir, 'train_files'))
+  shutil.copy('trainval_net.py', os.path.join(output_dir, 'train_files'))  # todo: modify this line when renaming the file
+  shutil.copy('lib/model/faster_rcnn', os.path.join(output_dir, 'train_files'))
 
   sampler_batch = sampler(train_size, args.batch_size)
 
